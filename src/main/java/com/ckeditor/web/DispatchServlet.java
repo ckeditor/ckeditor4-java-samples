9d0e32f5b39dbb7db72ab354ca5a4f291f84c40a
AnnaTomanek <a.tomanek@cksource.com>
2014-12-16 17:46:38 +0100

    Final corrections in API docs.

--- src/main/java/com/ckeditor/web/DispatchServlet.java
+++ src/main/java/com/ckeditor/web/DispatchServlet.java
@@ -20,102 +20,105 @@ import com.ckeditor.CKEditor;
  */
 public class DispatchServlet extends HttpServlet {
 
-	/** Serial identifier. */
-	private static final long serialVersionUID = 1L;
+    /**
+     * Serial identifier.
+     */
+    private static final long serialVersionUID = 1L;
 
-	/** Extension handled by this servlet. */
-	private static final String EXTENSION = "sample";
+    /**
+     * Extension handled by this servlet.
+     */
+    private static final String EXTENSION = "sample";
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost( HttpServletRequest request,
-			HttpServletResponse response ) throws ServletException, IOException {
-		doGet( request, response );
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet( HttpServletRequest request,
-			HttpServletResponse response ) throws ServletException, IOException {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
 
-		String requestPath = request.getRequestURI( );
+        String requestPath = request.getRequestURI();
 
-		if ( requestPath.endsWith( EXTENSION ) ) {
-			String uri = request.getRequestURI( );
+        if (requestPath.endsWith(EXTENSION)) {
+            String uri = request.getRequestURI();
 
-			String jspPath = uri.substring(
-					request.getContextPath( ).length( ),
-					( uri.length( ) - EXTENSION.length( ) ) )
-					+ "jsp";
+            String jspPath = uri.substring(
+                    request.getContextPath().length(),
+                    (uri.length() - EXTENSION.length()))
+                    + "jsp";
 
-			String requestFile = jspPath
-					.substring( jspPath.lastIndexOf( "/" ) + 1 );
+            String requestFile = jspPath
+                    .substring(jspPath.lastIndexOf("/") + 1);
 
-			provideConfiguration( requestFile, request );
+            provideConfiguration(requestFile, request);
 
-			// response.sendRedirect( jspPath );
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher( jspPath );
-			dispatcher.forward( request, response );
-		} else {
-			response.sendRedirect( "index.jsp" );
-		}
-	}
+            // response.sendRedirect( jspPath );
+            RequestDispatcher dispatcher = request
+                    .getRequestDispatcher(jspPath);
+            dispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("index.jsp");
+        }
+    }
 
-	/**
-	 * Puts appropriate configuration options, based on file name, in scope so
-	 * that they are available for JavaServer Pages to use.
-	 * 
-	 * @param requestFile
-	 *            a string representing the JSP file name for which the configuration
-	 *            should be provided.
-	 */
-	private void provideConfiguration( String requestFile,
-			HttpServletRequest request ) {
-		if ( requestFile.equals( "replaceTag.jsp" ) ) {
-			request.setAttribute( "cke_config",
-					ConfigurationProvider.getFirstEditorConfig( ) );
+    /**
+     * Puts appropriate configuration options, based on file name, in scope so
+     * that they are available for JSP to use.
+     *
+     * @param requestFile a string representing the JSP file name for which the
+     * configuration should be provided.
+     */
+    private void provideConfiguration(String requestFile,
+            HttpServletRequest request) {
+        if (requestFile.equals("replaceTag.jsp")) {
+            request.setAttribute("cke_config",
+                    ConfigurationProvider.getFirstEditorConfig());
 
-		} else if ( requestFile.equals( "replaceAllTag.jsp" ) ) {
-			request.setAttribute( "cke_config",
-					ConfigurationProvider.getSecondEditorConfig( ) );
+        } else if (requestFile.equals("replaceAllTag.jsp")) {
+            request.setAttribute("cke_config",
+                    ConfigurationProvider.getSecondEditorConfig());
 
-		} else if ( requestFile.equals( "insertTag.jsp" ) ) {
-			request.setAttribute( "cke_config",
-					ConfigurationProvider.getThirdEditorConfig( ) );
-			request.setAttribute( "cke_textarea_attrs",
-					ConfigurationProvider.getTextareaAttrs( ) );
+        } else if (requestFile.equals("insertTag.jsp")) {
+            request.setAttribute("cke_config",
+                    ConfigurationProvider.getThirdEditorConfig());
+            request.setAttribute("cke_textarea_attrs",
+                    ConfigurationProvider.getTextareaAttrs());
 
-		} else if ( requestFile.equals( "insertInlineTag.jsp" ) ) {
-			request.setAttribute( "cke_init_value",
-					ConfigurationProvider.getInitValue( ) );
+        } else if (requestFile.equals("insertInlineTag.jsp")) {
+            request.setAttribute("cke_init_value",
+                    ConfigurationProvider.getInitValue());
 
-		} else if ( requestFile.equals( "inlineTag.jsp" ) ) {
-			request.setAttribute( "cke_config",
-					ConfigurationProvider.getSecondEditorConfig( ) );
+        } else if (requestFile.equals("inlineTag.jsp")) {
+            request.setAttribute("cke_config",
+                    ConfigurationProvider.getSecondEditorConfig());
 
-		} else if ( requestFile.equals( "replaceObjectEl.jsp" ) ) {
-			CKEditor ckeditor = new CKEditor( request, "../ckeditor/",
-					CKEditor.Methods.Single.REPLACE, "editor1",
-					ConfigurationProvider.getFirstEditorConfig( ) );
-			request.setAttribute( "cke_instance", ckeditor );
+        } else if (requestFile.equals("replaceObjectEl.jsp")) {
+            CKEditor ckeditor = new CKEditor(request, "../ckeditor/",
+                    CKEditor.Methods.Single.REPLACE, "editor1",
+                    ConfigurationProvider.getFirstEditorConfig());
+            request.setAttribute("cke_instance", ckeditor);
 
-		} else if ( requestFile.equals( "events.jsp" ) ) {
-			request.setAttribute( "cke_global_events",
-					ConfigurationProvider.getGlobalEvent( ) );
+        } else if (requestFile.equals("events.jsp")) {
+            request.setAttribute("cke_global_events",
+                    ConfigurationProvider.getGlobalEvent());
 
-		} else if ( requestFile.equals( "timestamp.jsp" ) ) {
-			request.setAttribute( "cke_timestamp",
-					( ( int ) ( Math.random( ) * 50 + 1 ) ) );
+        } else if (requestFile.equals("timestamp.jsp")) {
+            request.setAttribute("cke_timestamp",
+                    ((int) (Math.random() * 50 + 1)));
 
-		} else if ( requestFile.equals( "globalConfig.jsp" ) ) {
-			request.setAttribute( "ckeditor_global_config",
-					ConfigurationProvider.getFourthEditorConfig( ) );
+        } else if (requestFile.equals("globalConfig.jsp")) {
+            request.setAttribute("ckeditor_global_config",
+                    ConfigurationProvider.getFourthEditorConfig());
 
-		}
-	}
+        }
+    }
 }
